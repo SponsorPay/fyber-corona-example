06c83ebe0682e55475bbac3d891c9891c40c1f3a
Emory Petermann <emory@heyzap.com>
2016-06-07 13:43:23 -0700

    working android apk with ads

--- Corona/main.lua
+++ Corona/main.lua
@@ -1,24 +1,92 @@
 local library = require "plugin.library"
 
--- This event is dispatched to the global Runtime object
--- by `didLoadMain:` in MyCoronaDelegate.mm
-local function delegateListener( event )
-	native.showAlert(
-		"Event dispatched from `didLoadMain:`",
-		"of type: " .. tostring( event.name ),
-		{ "OK" } )
-end
-Runtime:addEventListener( "delegate", delegateListener )
-
--- This event is dispatched to the following Lua function
--- by PluginLibrary::show() in PluginLibrary.mm
-local function listener( event )
-	print( "Received event from Library plugin (" .. event.name .. "): ", event.message )
-end
-
-library.init( listener )
-
 timer.performWithDelay( 1000, function()
-	library.show( "corona" )
+  library.loadFyberInterstitial("init", "41277", "ea74c5d4122aadd627ab856cea66fe91")
 end )
 
+-----------------------------------------------------------------------------------------
+local function on_InterstitialEvent(event)
+  -- load status : load_interstitial_available, load_interstitial_not_available, load_interstitial_request_error
+  -- show status : show_interstitial_error , show_interstitial_closed
+  -- extras : ReasonUserClickedOnAd, ReasonUserClosedAd
+end
+
+-----------------------------------------------------------------------------------------
+local function on_VideoEvent(event)
+  -- load status : load_video_available , load_video_not_available , load_video_request_error
+  -- show status : show_video_finished , show_video_aborted , show_video_error
+end
+
+--listeners---------------------------------------------------------------------------------------
+local function load_interstitial_listener(event)
+  if event.phase == "began" then
+    print("load_interstitial")
+
+    library.loadFyberInterstitial("load_interstitial")
+  end
+
+  return true
+end
+
+-----------------------------------------------------------------------------------------
+local function load_video_listener(event)
+  if event.phase == "began" then
+    print("load_video")
+
+    library.loadFyberInterstitial("load_video")
+  end
+
+  return true
+end
+
+-----------------------------------------------------------------------------------------
+local function show_interstitial_listener(event)
+  if event.phase == "began" then
+    print("show_interstitial")
+
+    library.loadFyberInterstitial("show_interstitial")
+  end
+
+  return true
+end
+
+-----------------------------------------------------------------------------------------
+local function show_video_listener(event)
+  if event.phase == "began" then
+    print("show_video")
+
+    library.loadFyberInterstitial("show_video")
+  end
+
+  return true
+end
+
+-----------------------------------------------------------------------------------------
+--register bridge listener
+Runtime:addEventListener("InterstitialEvent",   on_InterstitialEvent)
+Runtime:addEventListener("VideoEvent",  on_VideoEvent)
+
+local center_x              = display.contentWidth * 0.5
+local center_y              = display.contentHeight * 0.5
+
+--UI---------------------------------------------------------------------------------------
+local rect_load_interstitial = display.newRect( center_x - 70, center_y - 80, 125, 100 )
+rect_load_interstitial:setFillColor( 1,0,1 )
+local text_interstitial = display.newText( "Load interstitial", rect_load_interstitial.x, rect_load_interstitial.y, native.systemFont, 16 )
+rect_load_interstitial:addEventListener( "touch", load_interstitial_listener )
+
+local rect_load_video = display.newRect( center_x + 70, center_y - 80, 125, 100 )
+rect_load_video:setFillColor( 1,0,1 )
+local text_interstitial = display.newText( "Load video", rect_load_video.x, rect_load_video.y, native.systemFont, 16 )
+rect_load_video:addEventListener( "touch", load_video_listener )
+
+-----------------------------------------------------------------------------------------
+local rect_show_interstitial = display.newRect( center_x - 70, center_y + 80, 125, 100 )
+rect_show_interstitial:setFillColor( 1,0,1 )
+local text_interstitial = display.newText( "Show interstitial", rect_show_interstitial.x, rect_show_interstitial.y, native.systemFont, 16 )
+rect_show_interstitial:addEventListener( "touch", show_interstitial_listener )
+
+local rect_show_video = display.newRect( center_x + 70, center_y + 80, 125, 100 )
+rect_show_video:setFillColor( 1,0,1 )
+local text_interstitial = display.newText( "Show video", rect_show_video.x, rect_show_video.y, native.systemFont, 16 )
+rect_show_video:addEventListener( "touch", show_video_listener )
--- android/plugin/src/main/java/plugin/library/LuaLoader.java
+++ android/plugin/src/main/java/plugin/library/LuaLoader.java
@@ -9,10 +9,6 @@
 // e.g. [Lua] require "plugin.library"
 package plugin.library;
 
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-import com.ansca.corona.CoronaActivity;
 import com.ansca.corona.CoronaEnvironment;
 import com.ansca.corona.CoronaLua;
 import com.ansca.corona.CoronaRuntime;
@@ -70,8 +66,7 @@ public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
 	public int invoke(LuaState L) {
 		// Register this plugin into Lua with the following functions.
 		NamedJavaFunction[] luaFunctions = new NamedJavaFunction[] {
-			new InitWrapper(),
-			new ShowWrapper(),
+            new FyberLuaFunction()
 		};
 		String libName = L.toString( 1 );
 		L.register(libName, luaFunctions);
@@ -147,130 +142,5 @@ public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
 		// Remove the Lua listener reference.
 		CoronaLua.deleteRef( runtime.getLuaState(), fListener );
 		fListener = CoronaLua.REFNIL;
-	}
-
-	/**
-	 * The following Lua function has been called:  library.init( listener )
-	 * <p>
-	 * Warning! This method is not called on the main thread.
-	 * @param L Reference to the Lua state that the Lua function was called from.
-	 * @return Returns the number of values to be returned by the library.init() function.
-	 */
-	@SuppressWarnings({"WeakerAccess", "SameReturnValue"})
-	public int init(LuaState L) {
-		int listenerIndex = 1;
-
-		if ( CoronaLua.isListener( L, listenerIndex, EVENT_NAME ) ) {
-			fListener = CoronaLua.newRef( L, listenerIndex );
-		}
-
-		return 0;
-	}
-
-	/**
-	 * The following Lua function has been called:  library.show( word )
-	 * <p>
-	 * Warning! This method is not called on the main thread.
-	 * @param L Reference to the Lua state that the Lua function was called from.
-	 * @return Returns the number of values to be returned by the library.show() function.
-	 */
-	@SuppressWarnings("WeakerAccess")
-	public int show(LuaState L) {
-		// Fetch a reference to the Corona activity.
-		// Note: Will be null if the end-user has just backed out of the activity.
-		CoronaActivity activity = CoronaEnvironment.getCoronaActivity();
-		if (activity == null) {
-			return 0;
-		}
-
-		// Fetch the first argument from the called Lua function.
-		String word = L.checkString( 1 );
-		if ( null == word ) {
-			word = "corona";
-		}
-
-		// Create web view on the main UI thread.
-		final String url = "http://dictionary.reference.com/browse/" + word;
-		activity.runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				// Fetch a reference to the Corona activity.
-				// Note: Will be null if the end-user has just backed out of the activity.
-				CoronaActivity activity = CoronaEnvironment.getCoronaActivity();
-				if (activity == null) {
-					return;
-				}
-
-				// Create and set up the web view.
-				WebView view = new WebView(activity);
-
-				// Prevent redirect which causes an external browser to be launched
-				// because some sites detect phone/tablet and redirect.
-				view.setWebViewClient(new WebViewClient() {
-					@Override
-					public boolean shouldOverrideUrlLoading(WebView view, String url) {
-						return false;
-					}
-				});
-
-				// Display the web view.
-				activity.getOverlayView().addView(view);
-				view.loadUrl( url );
-			}
-		} );
-
-		return 0;
-	}
-
-	/** Implements the library.init() Lua function. */
-	@SuppressWarnings("unused")
-	private class InitWrapper implements NamedJavaFunction {
-		/**
-		 * Gets the name of the Lua function as it would appear in the Lua script.
-		 * @return Returns the name of the custom Lua function.
-		 */
-		@Override
-		public String getName() {
-			return "init";
-		}
-
-		/**
-		 * This method is called when the Lua function is called.
-		 * <p>
-		 * Warning! This method is not called on the main UI thread.
-		 * @param L Reference to the Lua state.
-		 *                 Needed to retrieve the Lua function's parameters and to return values back to Lua.
-		 * @return Returns the number of values to be returned by the Lua function.
-		 */
-		@Override
-		public int invoke(LuaState L) {
-			return init(L);
-		}
-	}
-
-	/** Implements the library.show() Lua function. */
-	@SuppressWarnings("unused")
-	private class ShowWrapper implements NamedJavaFunction {
-		/**
-		 * Gets the name of the Lua function as it would appear in the Lua script.
-		 * @return Returns the name of the custom Lua function.
-		 */
-		@Override
-		public String getName() {
-			return "show";
-		}
-
-		/**
-		 * This method is called when the Lua function is called.
-		 * <p>
-		 * Warning! This method is not called on the main UI thread.
-		 * @param L Reference to the Lua state.
-		 *                 Needed to retrieve the Lua function's parameters and to return values back to Lua.
-		 * @return Returns the number of values to be returned by the Lua function.
-		 */
-		@Override
-		public int invoke(LuaState L) {
-			return show(L);
-		}
 	}
 }
